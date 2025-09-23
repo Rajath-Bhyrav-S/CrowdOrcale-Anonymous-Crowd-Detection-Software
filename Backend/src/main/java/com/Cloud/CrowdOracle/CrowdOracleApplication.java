746280c11ccbe7224e09ecf01a99893868d41edb
Rajath-Bhyrav-S <rajathbhyrav147@outlook.com>
2025-09-23 19:12:51 +0530

    Backend Template - Java Spring

---
+++ Backend/src/main/java/com/Cloud/CrowdOracle/CrowdOracleApplication.java
@@ -0,0 +1,13 @@
+package com.Cloud.CrowdOracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdOracleApplication.class, args);
+	}
+
+}
